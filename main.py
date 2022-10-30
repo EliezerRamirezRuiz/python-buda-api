15d35259993163632527bd6db053d2f4e3bf7211
Eli-ezer Ramirez <ramirez.ruiz.eliezer.reuven@gmail.com>
2022-10-30 12:42:42 -0300

    upload the basic program

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+
+
+phrases = ['BTC-CLP', 'BTC-COP', 'BTC-USDC', 'USDC-ARS',
+           'USDC-PEN', 'USDC-COP', 'USDC-CLP', 'LTC-ARS', 'LTC-PEN',
+           'LTC-COP', 'LTC-CLP', 'LTC-BTC', 'BCH-ARS', 'ETH-ARS', 'BTC-ARS',
+           'BCH-PEN', 'BCH-COP', 'BCH-CLP', 'BCH-BTC', 'ETH-COP', 'ETH-PEN',
+           'BTC-PEN', 'ETH-BTC', 'ETH-CLP']
+
+
+def main():
+    var_boolean = True
+
+
+    while var_boolean:
+        i = input('Write the option do you want to get [HELP/help] / [Money in bitcoin]:').upper()
+        try:
+            if i in phrases:
+                url = f'https://www.buda.com/api/v2/markets/{i}'
+                response = requests.get(url)
+                print(response.json())
+                print(response.id)
+
+            elif i == 'HELP':
+                for item in phrases:
+                   print(item)
+
+            elif i == 'EXIT':
+                var_boolean = False
+                exit()
+
+            else:
+                print('Response invalid')
+
+        except:
+            print('')
+
+
+if __name__ == '__main__':
+    main()
